809b26e3da64a43d325214a2008bb7482a9bef79
jude4264 <jude4264@gmail.com>
2022-11-10 20:59:40 +0900

    [UPDATE] Rows 생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
+import request from './api/request';
 import './App.css';
 import Banner from './components/Banner';
 import Nav from './components/Nav';
+import Row from './components/Row';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Nav/>
       <Banner/>
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="id"
+        fetchUrl={request.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row
+        title="Trending Now"
+        id="TN"
+        fetchUrl={request.fetchTrending}
+      />
+      <Row
+       title="Top Rated"
+       id="TR"
+       fetchUrl={request.fetchTopRated}
+      />
+      <Row
+       title="Action Movies"
+       id="AM"
+       fetchUrl={request.fetchActionMovies}
+       />
+      <Row
+       title="Comedy Movies"
+       id="CM"
+       fetchUrl={request.fetchComedyMovies}
+      />
     </div>
   );
 }
