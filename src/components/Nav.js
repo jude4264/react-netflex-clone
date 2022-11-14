b7d30fe643b2ef9b4649b49cce7bf352aa4ef280
jude4264 <jude4264@gmail.com>
2022-11-14 20:19:10 +0900

    [UPDATE] 배포

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -38,7 +38,7 @@ const Nav = () => {
         alt="Netflix logo"
         src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png'
         className="nav_logo"
-        onClick={() => window.location.reload()}
+        onClick={() => (window.location.href = "/react-netflex-clone")}
       />
 
       <input
