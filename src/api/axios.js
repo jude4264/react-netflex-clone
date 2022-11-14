f93f83541cc24e2296e5231f7f99fe61e6d88cbf
jude4264 <jude4264@gmail.com>
2022-11-14 19:50:01 +0900

    [UPDATE] 슬라이더

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL : "https://api.themoviedb.org/3",
     params : {
-        api_key : "a104bdc5b76fa499f7771e953f77c92c",
+        api_key : process.env.REACT_APP_MOVIE_DB_API_KEY,
         language : "ko-KR"
     }
 })
--- src/components/Row.js
+++ src/components/Row.js
@@ -3,6 +3,17 @@ import React, { useEffect, useState } from 'react'
 import "./Row.css"
 import MovieModal from './MovieModal/MovieModal';
 
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 const Row = ({
     title,
     id,
@@ -36,28 +47,60 @@ const Row = ({
     return (
         <section className='row'>
             <h2>{title}</h2>
-            <div className='slider'>
+            {/* <div className='slider'>
                 <div className='slider__arrow-left'>
                     <span className="arrow"
                     onClick={()=>{
                         document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-                        /*document.getElementById(id).scrollLeft -= window.innerWidth-80;*/
                     }}
                     >{"<"}</span>
-                </div>
+                </div> */}
+            <Swiper
+                // install Swiper modules
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                // spaceBetween={50}
+                // slidesPerView={3}
+                navigation
+                pagination={{ clickable: true }}
+                loop={true}
+                breakpoints={{
+                    1378:{
+                        slidesPerView: 6,
+                        slidesPerGroup: 6
+                    },
+                    998:{
+                        slidesPerView: 5,
+                        slidesPerGroup: 5
+                    },
+                    625:{
+                        slidesPerView: 4,
+                        slidesPerGroup: 4
+                    },
+                    0:{
+                        slidesPerView: 3,
+                        slidesPerGroup: 3
+                    },
+                }}
+                // scrollbar={{ draggable: true }}
+                // onSwiper={(swiper) => console.log(swiper)}
+                // onSlideChange={() => console.log('slide change')}
+            >
                 <div id={id} className="row__posters">
                     {movies.map((movie) => (
-                        <img
-                            key={movie.id}
-                            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                            src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path
-                                } `}
-                            alt={movie.name}
-                            onClick={()=>handleClick(movie)}
-                        />
+                        <SwiperSlide>
+                            <img
+                                key={movie.id}
+                                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                                src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path
+                                    } `}
+                                alt={movie.name}
+                                onClick={() => handleClick(movie)}
+                            />
+                        </SwiperSlide>
                     ))}
                 </div>
-                <div className='slider__arrow-right'>
+            </Swiper>
+                {/* <div className='slider__arrow-right'>
                     <span 
                         className="arrow"
                         onClick={() => {
@@ -66,7 +109,7 @@ const Row = ({
                         {">"}
                     </span>
                 </div>
-            </div>
+            </div> */}
                         {modalOpen && (
                             <MovieModal {...movieSelected} setmodalOpen={setmodalOpen}/>
                         )}
